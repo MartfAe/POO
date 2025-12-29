6ca9a0c61d866312b809ffcaf926a5db1514b971
MartfAe <anaemilia@aluno.ufrb.edu.br>
2025-12-29 13:01:22 -0300

    Inicializando o trabalho final da dsiciplina

---
+++ Trabalho Final/DAO/VehicleDAO.java
@@ -0,0 +1,12 @@
+package DAO;
+import java.util.List;
+
+import model.Vehicle;
+
+import java.util.ArrayList;
+
+public class VehicleDAO {
+
+    private List<Vehicle> vehicleList = new ArrayList<>();
+
+}
---
+++ Trabalho Final/model/Customer.java
@@ -0,0 +1,12 @@
+package model;
+public class Client {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    
+
+
+
+}
