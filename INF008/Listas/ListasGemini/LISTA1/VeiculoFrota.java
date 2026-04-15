6981136f7b5a92d9c5f57d510cbd158143e16fdc
MartfAe <anaemilia@aluno.ufrb.edu.br>
2026-04-15 19:57:49 -0300

    Add cóigo de veiculo

--- INF008/Listas/ListasGemini/LISTA1/VeiculoFrota.java
+++ INF008/Listas/ListasGemini/LISTA1/VeiculoFrota.java
@@ -18,6 +18,46 @@ public class VeiculoFrota {
         this(placa, modelo, capacidadeCarga, 0);
     }
 
+    public VeiculoFrota(String placa, String modelo){
+        this(placa, modelo, 0, 0.0);
+    }
+
+    public void adcionarCarga(int quantidade){
+        if(validarqtdCarga(quantidade)){
+            setCargaCarregada(this.cargaCarregada + quantidade);
+            System.out.println("Carga atualizada: " + getCargaCarregada());
+        }
+    }
+
+
+    public void descarregar(int qtdCarga){
+       int valorParaRemover = -Math.abs(qtdCarga)
+       if(validarqtdCarga(valorParaRemover)){
+        setCargaCarregada(this.cargaCarregada + valorParaRemover);
+        System.out.println("Quantidade de carga atual:"+getCargaCarregada());
+       }
+        
+    }
+
+    public boolean validarqtdCarga(int qtdCarga){
+        if(qtdCarga>0){
+            if((this.cargaCarregada + qtdCarga)>this.capacidadeCarga){
+                System.out.println("ERRO: Quantidade máxima ultrapassada!");
+                return false;
+            }
+        }else if(qtdCarga<0){
+            if (Math.abs(qtdCarga)>this.cargaCarregada){
+                System.out.println("ERRO: Não a há carga carregada suficiente");
+                return false;
+            }
+        }else{
+            System.out.println("A quantidade de carga não pode ser zero");
+            return false;
+        }
+
+        return true;
+    }
+
     public int getCapacidadeCarga(){
         return capacidadeCarga;
     }
@@ -27,27 +67,8 @@ public class VeiculoFrota {
 
     }
 
-
-
-
-    public VeiculoFrota(String placa, String modelo){
-        this(placa, modelo, 0, 0.0);
-    }
-
-    public void adcionarCarga(int qtdCarga){
-        if(qtdCarga<=0){
-            System.out.println("ERRO: Valor inválida, a qtd de carga não pode ser menor ou igual a zero.");
-            return;
-        }else if((this.cargaCarregada + qtdCarga)>this.capacidadeCarga){
-            System.out.println("ERRO: Capacidade de carga ultrapassada.");
-        }else{
-            this.cargaCarregada += qtdCarga;
-            System.out.println("Carga carregada com sucesso");
-        }
-    }
-
-    public void descarregar(int qtdCarga){
-        
+    public void setCargaCarregada(int quantidadeCarga){
+        this.cargaCarregada = quantidadeCarga;
     }
 
 }
