2835d4faa617c98c0192eaa1aba216c24d816ab6
Ana Emília Martfeld <anaemilia@aluno.ufrb.edu.br>
2026-04-15 14:42:55 +0000

    Att veiculo frota

--- INF008/Listas/ListasGemini/LISTA1/VeiculoFrota.java
+++ INF008/Listas/ListasGemini/LISTA1/VeiculoFrota.java
@@ -3,6 +3,7 @@ public class VeiculoFrota {
     private String placa;
     private String modelo;
     private int capacidadeCarga;
+    private int cargaCarregada;
     private double quilometragem;
 
 
@@ -17,15 +18,36 @@ public class VeiculoFrota {
         this(placa, modelo, capacidadeCarga, 0);
     }
 
+    public int getCapacidadeCarga(){
+        return capacidadeCarga;
+    }
+
+    public int getCargaCarregada(){
+        return cargaCarregada;
+
+    }
+
+
+
+
     public VeiculoFrota(String placa, String modelo){
         this(placa, modelo, 0, 0.0);
+    }
 
     public void adcionarCarga(int qtdCarga){
         if(qtdCarga<=0){
-            System.out.println("Valor inválido.");
-    }else{
+            System.out.println("ERRO: Valor inválida, a qtd de carga não pode ser menor ou igual a zero.");
+            return;
+        }else if((this.cargaCarregada + qtdCarga)>this.capacidadeCarga){
+            System.out.println("ERRO: Capacidade de carga ultrapassada.");
+        }else{
+            this.cargaCarregada += qtdCarga;
+            System.out.println("Carga carregada com sucesso");
+        }
+    }
+
+    public void descarregar(int qtdCarga){
         
     }
-}
 
 }
